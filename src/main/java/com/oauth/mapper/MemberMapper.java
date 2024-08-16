f227a69a372bb133110bf453f8ba5f7fd8d54b39
Kim971202 <kimyohan971202@gmail.com>
2024-08-16 16:07:07 +0900

    수정내용
    - 테스트 Log 추가

--- src/main/java/com/oauth/mapper/MemberMapper.java
+++ src/main/java/com/oauth/mapper/MemberMapper.java
@@ -24,6 +24,7 @@ public interface MemberMapper {
     public List<AuthServerDTO> getPushCodeStatus(@Param("userId") String params, @Param("deviceIds") String deviceIds);
     public List<AuthServerDTO> getWorkTime(AuthServerDTO param);
     public List<AuthServerDTO> getUserIdsByDeviceId(String deviceId);
+    public List<AuthServerDTO> getPushYnStatusByUserIds(List<AuthServerDTO> userIds);
     public AuthServerDTO getPushYnStatus(String userId);
     public AuthServerDTO getUserByUserId(String userId);
     public AuthServerDTO getAccountByUserId(String userId);
