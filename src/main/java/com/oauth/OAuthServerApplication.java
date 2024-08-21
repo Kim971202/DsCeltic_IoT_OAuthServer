5db6031efc92b1a4f4fdee8024b1bcc4f8ab1c70
Kim971202 <kimyohan971202@gmail.com>
2024-08-21 17:00:36 +0900

    수정내용
    - 메모리 사용량 관련 모니티링 클래스 추가
    
    
    Former-commit-id: 42ae6e8ab1a518fcb520c53510193a036cbe3ca3

--- src/main/java/com/oauth/OAuthServerApplication.java
+++ src/main/java/com/oauth/OAuthServerApplication.java
@@ -1,7 +1,10 @@
 package com.oauth;
 
+import com.oauth.config.MemoryMonitor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"com.oauth.*"})
 public class OAuthServerApplication {
@@ -10,4 +13,11 @@ public class OAuthServerApplication {
 		SpringApplication.run(OAuthServerApplication.class, args);
 	}
 
+	// CommandLineRunner 사용하여 애플리케이션 시작 후 메모리 모니터링
+	@Bean
+	public CommandLineRunner memoryMonitorRunner() {
+		return args -> {
+			MemoryMonitor.logMemoryUsage();
+		};
+	}
 }
---
+++ src/main/java/com/oauth/config/ScheduledMemoryMonitor.java
@@ -0,0 +1,14 @@
+package com.oauth.config;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledMemoryMonitor {
+
+    // 10초마다 메모리 상태를 출력
+    @Scheduled(fixedRate = 10000)
+    public void monitorMemory() {
+        MemoryMonitor.logMemoryUsage();
+    }
+}
