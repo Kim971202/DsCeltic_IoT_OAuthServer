42ae6e8ab1a518fcb520c53510193a036cbe3ca3
Kim971202 <kimyohan971202@gmail.com>
2024-08-21 17:00:36 +0900

    수정내용
    - 메모리 사용량 관련 모니티링 클래스 추가

---
+++ src/main/java/com/oauth/config/MemoryMonitor.java
@@ -0,0 +1,16 @@
+package com.oauth.config;
+
+public class MemoryMonitor {
+    public static void logMemoryUsage() {
+        Runtime runtime = Runtime.getRuntime();
+        long totalMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+        long usedMemory = totalMemory - freeMemory;
+        long maxMemory = runtime.maxMemory();
+
+        System.out.println("Used Memory: " + usedMemory / (1024 * 1024) + " MB");
+        System.out.println("Free Memory: " + freeMemory / (1024 * 1024) + " MB");
+        System.out.println("Total Memory: " + totalMemory / (1024 * 1024) + " MB");
+        System.out.println("Max Memory: " + maxMemory / (1024 * 1024) + " MB");
+    }
+}
