6b1bbba3179ff8acf7a4dc342a7fec716f930c57
Kim971202 <kimyohan971202@gmail.com>
2024-10-19 11:33:12 +0900

    수정내용
    - Google에서 오는 예약 요청 관련 수정 (24시간 예약)

--- src/main/java/com/oauth/controller/GoogleController.java
+++ src/main/java/com/oauth/controller/GoogleController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oauth.dto.AuthServerDTO;
 import com.oauth.dto.gw.DeviceStatusInfo;
 import com.oauth.dto.gw.Set12;
+import com.oauth.dto.gw.Set24;
 import com.oauth.mapper.DeviceMapper;
 import com.oauth.service.impl.MobiusService;
 import com.oauth.utils.Common;
@@ -15,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
@@ -47,6 +49,12 @@ public class GoogleController {
 
         // 12시간 예약: 분
         String workTime = "";
+
+        // 24시간 타입
+        String md = "";
+
+        // 24시간 시간 List
+        String hs = "";
 
         String value = common.readCon(jsonBody, "value");
         String userId = common.readCon(jsonBody, "userId");
@@ -99,23 +107,23 @@ public class GoogleController {
         // redisCommand.setValues(conMap.get("uuId"), redisValue);
         mobiusService.createCin(deviceArray[6], userId, JSON.toJson(conMap));
 
+
         // TODO: 예약의 경우 opMd 후 예약 SET 까지 처리 해야 RC 화면 변경 가능 (구형 기준)
         // 10 - 24시간
         // 11 - 반복예약
         // 12 - 주간예약
-        if(value.equals("11")){
-            DeviceStatusInfo.Device deviceInfo = deviceMapper.getSingleDeviceStauts(deviceId);
-            Set12 set12 = new Set12();
+        DeviceStatusInfo.Device deviceInfo = deviceMapper.getSingleDeviceStauts(deviceId);
+        // ObjectMapper 인스턴스 생성
+        ObjectMapper objectMapper = new ObjectMapper();
+        ConcurrentHashMap<String, Object> map = new ConcurrentHashMap<String, Object>();
 
-            // ObjectMapper 인스턴스 생성
-            ObjectMapper objectMapper = new ObjectMapper();
+        if(value.equals("11")){
+            Set12 set12 = new Set12();
 
             try {
                 JsonNode jsonNode = objectMapper.readTree(deviceInfo.getH12());
-
                 workPeriod = jsonNode.get("hr").asText();
                 workTime = jsonNode.get("mn").asText();
-
             } catch (Exception e){
                 log.error("", e);
             }
@@ -130,6 +138,28 @@ public class GoogleController {
             set12.setUuId(common.getTransactionId());
 
             mobiusService.createCin(deviceArray[6], userId, JSON.toJson(set12));
+        } else if(value.equals("10")){
+            Set24 set24 = new Set24();
+
+            try {
+                JsonNode jsonNode = objectMapper.readTree(deviceInfo.getH24());
+                md = jsonNode.get("md").asText();
+                hs = jsonNode.get("md").asText();
+
+            } catch (Exception e){
+                log.error("", e);
+            }
+            set24.setAccessToken(common.getTransactionId());
+            set24.setUserId(userId);
+            set24.setDeviceId(deviceId);
+            set24.setControlAuthKey("0000");
+            set24.setHours(hs);
+            set24.setType24h(md);
+            set24.setOnOffFlag("on");
+            set24.setFunctionId("24h");
+            set24.setUuId(common.getTransactionId());
+
+            mobiusService.createCin(deviceArray[6], userId, JSON.toJson(set24));
         }
 
         return "OK";
