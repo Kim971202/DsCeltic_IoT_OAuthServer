6dc5205b0ab7b026970b9b9111ddf6ab6b277ae6
Kim971202 <kimyohan971202@gmail.com>
2024-11-05 10:05:15 +0900

    수정내용
    - ApiResponse.Data toString()함수 추가

--- src/main/java/com/oauth/response/ApiResponse.java
+++ src/main/java/com/oauth/response/ApiResponse.java
@@ -2,6 +2,7 @@ package com.oauth.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -183,6 +184,15 @@ public class ApiResponse {
             private String errorCode;
             private String errorDateTime;
             private String serialNumber;
+        }
+        @Override
+        public String toString() {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+            } catch (Exception e) {
+                return "Error converting to JSON: " + e.getMessage();
+            }
         }
     }
 }
