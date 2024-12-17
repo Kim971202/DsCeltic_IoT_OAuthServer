396c706cf43f9fabb9b8ad8beea21e8cdd86c436
Kim971202 <kimyohan971202@gmail.com>
2024-12-17 15:24:58 +0900

    수정내용
    - sRow, numOfRow 기본값 변경

--- src/main/java/com/oauth/dto/AuthServerDTO.java
+++ src/main/java/com/oauth/dto/AuthServerDTO.java
@@ -25,9 +25,9 @@ public class AuthServerDTO implements Serializable {
 
     private static final long serialVersionUID = 54436712726576487L;
 
-    private Integer pageNo = 2;
+    private Integer pageNo = 1;
     private Integer numOfRows = 1;
-    private Integer sRow = 1;
+    private Integer sRow = 0;
 
 
     private Long idx;
