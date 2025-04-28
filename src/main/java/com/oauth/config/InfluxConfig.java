b90148e89579baa887a30a4cc1550409935df6e3
Kim971202 <kimyohan971202@gmail.com>
2025-04-28 14:20:24 +0900

    - influx DB 로직 주석 처리

--- src/main/java/com/oauth/config/InfluxConfig.java
+++ src/main/java/com/oauth/config/InfluxConfig.java
@@ -1,41 +1,41 @@
-package com.oauth.config;
-
-import com.influxdb.client.InfluxDBClient;
-import com.influxdb.client.InfluxDBClientFactory;
-import com.influxdb.client.QueryApi;
-import com.influxdb.client.WriteApi;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class InfluxConfig {
-
-    @Value("${influx.url}")
-    private String influxUrl;
-
-    @Value("${influx.token}")
-    private String token;
-
-    @Value("${influx.org}")
-    private String org;
-
-    @Value("${influx.bucket}")
-    private String bucket;
-
-    @Bean
-    public InfluxDBClient influxDBClient() {
-        return InfluxDBClientFactory
-                .create(influxUrl, token.toCharArray(), org, bucket);
-    }
-
-    @Bean
-    public WriteApi writeApi(InfluxDBClient client) {
-        return client.getWriteApi();
-    }
-
-    @Bean
-    public QueryApi queryApi(InfluxDBClient client) {
-        return client.getQueryApi();
-    }
-}
+//package com.oauth.config;
+//
+//import com.influxdb.client.InfluxDBClient;
+//import com.influxdb.client.InfluxDBClientFactory;
+//import com.influxdb.client.QueryApi;
+//import com.influxdb.client.WriteApi;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class InfluxConfig {
+//
+//    @Value("${influx.url}")
+//    private String influxUrl;
+//
+//    @Value("${influx.token}")
+//    private String token;
+//
+//    @Value("${influx.org}")
+//    private String org;
+//
+//    @Value("${influx.bucket}")
+//    private String bucket;
+//
+//    @Bean
+//    public InfluxDBClient influxDBClient() {
+//        return InfluxDBClientFactory
+//                .create(influxUrl, token.toCharArray(), org, bucket);
+//    }
+//
+//    @Bean
+//    public WriteApi writeApi(InfluxDBClient client) {
+//        return client.getWriteApi();
+//    }
+//
+//    @Bean
+//    public QueryApi queryApi(InfluxDBClient client) {
+//        return client.getQueryApi();
+//    }
+//}
--- src/main/java/com/oauth/controller/InfluxController.java
+++ src/main/java/com/oauth/controller/InfluxController.java
@@ -1,21 +1,21 @@
 package com.oauth.controller;
 
-import com.oauth.service.impl.InfluxService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import com.oauth.service.impl.InfluxService;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class InfluxController {
-
-    private final InfluxService svc;
-
-    public InfluxController(InfluxService svc) {
-        this.svc = svc;
-    }
-
-    @GetMapping("/influx/write")
-    public String write() {
-        svc.writeMeasurement();
-        return "written";
-    }
-}
+//@RestController
+//public class InfluxController {
+//
+//    private final InfluxService svc;
+//
+//    public InfluxController(InfluxService svc) {
+//        this.svc = svc;
+//    }
+//
+//    @GetMapping("/influx/write")
+//    public String write() {
+//        svc.writeMeasurement();
+//        return "written";
+//    }
+//}
