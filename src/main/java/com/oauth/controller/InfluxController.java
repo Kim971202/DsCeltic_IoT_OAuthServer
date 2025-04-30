63a31ea002b91711a93c582b81626eda6b41993c
Kim971202 <kimyohan971202@gmail.com>
2025-04-30 09:16:58 +0900

    - influxdb token값 수정

--- src/main/java/com/oauth/controller/InfluxController.java
+++ src/main/java/com/oauth/controller/InfluxController.java
@@ -22,8 +22,8 @@ public class InfluxController {
                 "controlCodeName",
                 "userId",
                 "devoiceId",
-                "codeType",
-                "commandFlow");
+                "2",
+                "2");
         return "written";
     }
 }
